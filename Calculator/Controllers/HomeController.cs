1f488ae1e63399150c99300cce9a4f193f505cdb
Alexandra Imereli <alexandra.imereli@gmail.com>
2017-04-05 14:05:10 +0300

    Добавлена обработка исключений

--- Calculator/Controllers/HomeController.cs
+++ Calculator/Controllers/HomeController.cs
@@ -22,20 +22,32 @@ namespace Calculator.Controllers
         [HttpPost]
         public ActionResult Calculate(decimal arg1, string operation, decimal arg2)
         {
-            CalcResult result = new CalcResult() {
-                arg1 = arg1,
-                arg2 = arg2,
-                operation = operation,
-                operationTime = DateTime.Now.TimeOfDay,
-                result = arg1 + arg2
-
-            };
-            if (ModelState.IsValid)
+            try
             {
-                db.CalcResults.Add(result);
-                db.SaveChanges();
-            }
+                decimal result = Result(arg1, arg2, operation);
 
+                CalcResult calcResult = new CalcResult()
+                {
+                    arg1 = arg1,
+                    arg2 = arg2,
+                    operation = operation,
+                    operationTime = DateTime.Now.TimeOfDay,
+                    result = result
+                };
+                if (ModelState.IsValid)
+                {
+                    db.CalcResults.Add(calcResult);
+                    db.SaveChanges();
+                    ViewBag.Result = result;
+                    ModelState.Clear();
+                    return View();
+                }
+            }catch(Exception ex)
+            {
+                ViewBag.Result = ex.Message;
+                ModelState.Clear();
+                
+            }
             return View();
         }
         
@@ -47,6 +59,19 @@ namespace Calculator.Controllers
             ViewBag.Results = results;
             // возвращаем представление
             return View();
+        }
+
+        private decimal Result(decimal argument1, decimal argument2, string operation)
+        {
+            switch (operation)
+            {
+                case "addition": return argument1 + argument2; 
+                case "subtraction": return argument1 - argument2;
+                case "multiplication": return argument1 * argument2;
+                case "division": return argument1 / argument2; 
+                default: return 0;
+                   
+            }
         }
     }
 }
